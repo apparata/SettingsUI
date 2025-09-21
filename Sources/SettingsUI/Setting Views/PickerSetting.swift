f8a637f32d3742a9a314277080e1aa225b751534
Martin Johannesson <mjohannesson@gmail.com>
2025-09-21 18:09:29 +0200

    Add ability to customize picker elements.

--- Sources/SettingsUI/Setting Views/PickerSetting.swift
+++ Sources/SettingsUI/Setting Views/PickerSetting.swift
@@ -35,10 +35,11 @@ import SwiftUI
 /// )
 /// ```
 ///
-public struct PickerSetting<Icon: View, Label: View, T: SettingPickable>: View {
+public struct PickerSetting<Icon: View, Label: View, CaseLabel: View, T: SettingPickable>: View {
 
     private let icon: Icon?
     private let label: Label
+    private let caseLabel: (T) -> CaseLabel
     private let values: [T]
     @Binding private var selection: T
 
@@ -48,23 +49,27 @@ public struct PickerSetting<Icon: View, Label: View, T: SettingPickable>: View {
         values: [T],
         selection: Binding<T>,
         @ViewBuilder icon: () -> Icon,
-        @ViewBuilder label: () -> Label
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
     ) {
         self.values = values
         self._selection = selection
         self.icon = icon()
         self.label = label()
+        self.caseLabel = caseLabel
     }
 
     public init(
         values: [T],
         selection: Binding<T>,
-        @ViewBuilder label: () -> Label
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
     ) {
         self.values = values
         self._selection = selection
         self.icon = nil
         self.label = label()
+        self.caseLabel = caseLabel
     }
 
     public var body: some View {
@@ -77,41 +82,28 @@ public struct PickerSetting<Icon: View, Label: View, T: SettingPickable>: View {
             Spacer()
         }) {
             ForEach(values) { value in
-                Text(value.description)
+                caseLabel(value)
                     .tag(value)
             }
         }
     }
 }
 
-// MARK: T: CaseIterable
+// MARK: CaseLabel: Text
 
-extension PickerSetting where T: CaseIterable {
+extension PickerSetting where CaseLabel == Text {
     public init(
+        values: [T],
         selection: Binding<T>,
         @ViewBuilder icon: () -> Icon,
         @ViewBuilder label: () -> Label
     ) {
-        self.values = Array(T.allCases)
+        self.values = values
         self._selection = selection
         self.icon = icon()
         self.label = label()
+        self.caseLabel = { Text($0.description) }
     }
-
-    public init(
-        selection: Binding<T>,
-        @ViewBuilder label: () -> Label
-    ) {
-        self.values = Array(T.allCases)
-        self._selection = selection
-        self.icon = nil
-        self.label = label()
-    }
-}
-
-// MARK: Icon: Never
-
-extension PickerSetting where Icon == Never {
 
     public init(
         values: [T],
@@ -122,10 +114,113 @@ extension PickerSetting where Icon == Never {
         self._selection = selection
         self.icon = nil
         self.label = label()
+        self.caseLabel = { Text($0.description) }
+    }
+}
+
+// MARK: T: CaseIterable
+
+extension PickerSetting where T: CaseIterable {
+    public init(
+        selection: Binding<T>,
+        @ViewBuilder icon: () -> Icon,
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.values = Array(T.allCases)
+        self._selection = selection
+        self.icon = icon()
+        self.label = label()
+        self.caseLabel = caseLabel
+    }
+
+    public init(
+        selection: Binding<T>,
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.values = Array(T.allCases)
+        self._selection = selection
+        self.icon = nil
+        self.label = label()
+        self.caseLabel = caseLabel
+    }
+}
+
+extension PickerSetting where T: CaseIterable, CaseLabel == Text {
+    public init(
+        selection: Binding<T>,
+        @ViewBuilder icon: () -> Icon,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.values = Array(T.allCases)
+        self._selection = selection
+        self.icon = icon()
+        self.label = label()
+        self.caseLabel = { Text($0.description) }
+    }
+
+    public init(
+        selection: Binding<T>,
+        @ViewBuilder label: () -> Label,
+    ) {
+        self.values = Array(T.allCases)
+        self._selection = selection
+        self.icon = nil
+        self.label = label()
+        self.caseLabel = { Text($0.description) }
+    }
+}
+
+// MARK: Icon: Never
+
+extension PickerSetting where Icon == Never {
+
+    public init(
+        values: [T],
+        selection: Binding<T>,
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.values = values
+        self._selection = selection
+        self.icon = nil
+        self.label = label()
+        self.caseLabel = caseLabel
+    }
+}
+
+extension PickerSetting where Icon == Never, CaseLabel == Text {
+
+    public init(
+        values: [T],
+        selection: Binding<T>,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.values = values
+        self._selection = selection
+        self.icon = nil
+        self.label = label()
+        self.caseLabel = { Text($0.description) }
     }
 }
 
 extension PickerSetting where Icon == Never, T: CaseIterable {
+
+    public init(
+        selection: Binding<T>,
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.values = Array(T.allCases)
+        self._selection = selection
+        self.icon = nil
+        self.label = label()
+        self.caseLabel = caseLabel
+    }
+}
+
+extension PickerSetting where Icon == Never, CaseLabel == Text, T: CaseIterable {
 
     public init(
         selection: Binding<T>,
@@ -135,13 +230,42 @@ extension PickerSetting where Icon == Never, T: CaseIterable {
         self._selection = selection
         self.icon = nil
         self.label = label()
+        self.caseLabel = { Text($0.description) }
     }
 }
-
 
 // MARK: Label: Text
 
 extension PickerSetting where Icon == Never, Label == Text {
+
+    public init(
+        _ titleKey: LocalizedStringKey,
+        values: [T],
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(values: values, selection: selection) {
+            Text(titleKey)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+
+    public init<S: StringProtocol>(
+        _ title: S,
+        values: [T],
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(values: values, selection: selection) {
+            Text(title)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+}
+
+extension PickerSetting where Icon == Never, Label == Text, CaseLabel == Text {
 
     public init(
         _ titleKey: LocalizedStringKey,
@@ -165,6 +289,33 @@ extension PickerSetting where Icon == Never, Label == Text {
 }
 
 extension PickerSetting where Icon == Never, Label == Text, T: CaseIterable {
+
+    public init(
+        _ titleKey: LocalizedStringKey,
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(selection: selection) {
+            Text(titleKey)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+
+    public init<S: StringProtocol>(
+        _ title: S,
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(selection: selection) {
+            Text(title)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+}
+
+extension PickerSetting where Icon == Never, Label == Text, CaseLabel == Text, T: CaseIterable {
 
     public init(
         _ titleKey: LocalizedStringKey,
@@ -193,17 +344,54 @@ extension PickerSetting where Icon == SettingIcon {
         values: [T],
         selection: Binding<T>,
         systemIcon: String,
-        @ViewBuilder label: () -> Label
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
     ) {
         self.init(values: values, selection: selection) {
                 SettingIcon(systemIcon)
             } label: {
                 label()
+            } caseLabel: {
+                caseLabel($0)
             }
     }
 }
 
+extension PickerSetting where Icon == SettingIcon, CaseLabel == Text {
+
+    public init(
+        values: [T],
+        selection: Binding<T>,
+        systemIcon: String,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.init(values: values, selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            label()
+        }
+    }
+}
+
 extension PickerSetting where Icon == SettingIcon, T: CaseIterable {
+
+    public init(
+        selection: Binding<T>,
+        systemIcon: String,
+        @ViewBuilder label: () -> Label,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            label()
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+}
+
+extension PickerSetting where Icon == SettingIcon, CaseLabel == Text, T: CaseIterable {
 
     public init(
         selection: Binding<T>,
@@ -226,6 +414,41 @@ extension PickerSetting where Icon == SettingIcon, Label == Text {
         _ titleKey: LocalizedStringKey,
         systemIcon: String,
         values: [T],
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(values: values, selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            Text(titleKey)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+
+    public init<S: StringProtocol>(
+        _ title: S,
+        systemIcon: String,
+        values: [T],
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(values: values, selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            Text(title)
+        } caseLabel: {
+            caseLabel($0)
+        }
+    }
+}
+
+extension PickerSetting where Icon == SettingIcon, Label == Text, CaseLabel == Text {
+
+    public init(
+        _ titleKey: LocalizedStringKey,
+        systemIcon: String,
+        values: [T],
         selection: Binding<T>
     ) {
         self.init(values: values, selection: selection) {
@@ -239,7 +462,7 @@ extension PickerSetting where Icon == SettingIcon, Label == Text {
         _ title: S,
         systemIcon: String,
         values: [T],
-        selection: Binding<T>,
+        selection: Binding<T>
     ) {
         self.init(values: values, selection: selection) {
             SettingIcon(systemIcon)
@@ -254,12 +477,15 @@ extension PickerSetting where Icon == SettingIcon, Label == Text, T: CaseIterabl
     public init(
         _ titleKey: LocalizedStringKey,
         systemIcon: String,
-        selection: Binding<T>
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
     ) {
         self.init(selection: selection) {
             SettingIcon(systemIcon)
         } label: {
             Text(titleKey)
+        } caseLabel: {
+            caseLabel($0)
         }
     }
 
@@ -267,15 +493,49 @@ extension PickerSetting where Icon == SettingIcon, Label == Text, T: CaseIterabl
         _ title: S,
         systemIcon: String,
         selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
     ) {
         self.init(selection: selection) {
             SettingIcon(systemIcon)
         } label: {
             Text(title)
+        } caseLabel: {
+            caseLabel($0)
         }
     }
 }
 
+extension PickerSetting where Icon == SettingIcon, Label == Text, CaseLabel == Text, T: CaseIterable {
+
+    public init(
+        _ titleKey: LocalizedStringKey,
+        systemIcon: String,
+        selection: Binding<T>
+    ) {
+        self.init(selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            Text(titleKey)
+        } caseLabel: {
+            Text($0.description)
+        }
+    }
+
+    public init<S: StringProtocol>(
+        _ title: S,
+        systemIcon: String,
+        selection: Binding<T>,
+        @ViewBuilder caseLabel: @escaping (T) -> CaseLabel
+    ) {
+        self.init(selection: selection) {
+            SettingIcon(systemIcon)
+        } label: {
+            Text(title)
+        } caseLabel: {
+            Text($0.description)
+        }
+    }
+}
 
 // MARK: - Preview
 
@@ -290,6 +550,13 @@ private enum APIEnvironment: String, SettingPickable, CaseIterable {
     var description: String {
         rawValue
     }
+    var icon: String {
+        switch self {
+        case .development: "hammer.fill"
+        case .staging: "squares.leading.rectangle"
+        case .production: "sparkles"
+        }
+    }
 }
 #endif
 
@@ -302,7 +569,10 @@ private enum APIEnvironment: String, SettingPickable, CaseIterable {
                 PickerSetting(
                     "Title",
                     values: APIEnvironment.allCases,
-                    selection: $selection
+                    selection: $selection,
+                    caseLabel: {
+                        Label($0.description, systemImage: $0.icon)
+                    }
                 )
                 PickerSetting(
                     "Title",
@@ -321,7 +591,7 @@ private enum APIEnvironment: String, SettingPickable, CaseIterable {
                     selection: $selection,
                     systemIcon: "shippingbox.fill"
                 ) {
-                    Text("Title").fontWeight(.semibold)
+                        Text("Title").fontWeight(.semibold)
                 }
             }
 
